500cfefa87e76f52a42749f70a876f84b007ffaf
Viet Nguyen <47221207+uvipen@users.noreply.github.com>
2022-08-08 14:00:42 +0200

    Update train_csgo.sh

--- train_csgo.sh
+++ train_csgo.sh
@@ -3,4 +3,4 @@ git submodule init
 git submodule update
 cp datasets/csgo.yaml yolov5/data/csgo.yaml
 cd yolov5
-python train.py --img 608 --batch 128 --epochs 100 --data csgo.yaml --weights yolov5n.pt --project ../trained_models/csgo
+python train.py --img 640 --batch 128 --epochs 100 --data csgo.yaml --weights yolov5n.pt --project ../trained_models/csgo
