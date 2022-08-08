61a4722cc01d09bac28c83d1f5b9cc6c7c29114b
VietNguyen <nhviet1009@gmail.com>
2022-08-08 10:43:21 +0200

    update sh

--- train_playing_cards.sh
+++ train_playing_cards.sh
@@ -3,4 +3,4 @@ git submodule init
 git submodule update
 cp datasets/playing_cards.yaml yolov5/data/playing_cards.yaml
 cd yolov5
-python train.py --img 640 --batch 128 --epochs 50 --data playing_cards.yaml --weights yolov5n.pt --single-cls
+python train.py --img 608 --batch 128 --epochs 100 --data playing_cards.yaml --weights yolov5n.pt --project ../trained_models/playing_cards
